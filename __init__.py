9b0b391af5b9d348a6fb377f990c666993f3ee35
jsv0ice <joel@zeinestone.com>
2024-01-10 15:51:28 -0600

    Implemented web socket connection instead of rest api for color changes.
    Had to add a few libraries to the project to make it work with HA, as there is a dependency conflict.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-import requests
 import aiohttp
 from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.helpers.entity import Entity
@@ -10,11 +9,16 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.core import _LOGGER
 import logging
+import sys
+import os
 
+currentdir = os.path.dirname(os.path.abspath(__file__))
+libs_path = os.path.join(currentdir, 'libs')
+sys.path.insert(0, libs_path)
 
+import socketio
 
 DOMAIN = "world_map_entity_manager"
-#API_URL = 'http://10.0.0.71:5000'
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -47,6 +51,24 @@ SET_COLOR_SCHEMA = vol.Schema({
     vol.Required("is_on"): cv.boolean,
 })
 
+async def async_setup_websocket(hass: HomeAssistant, host, port):
+    sio = socketio.AsyncClient(logger=_LOGGER)
+
+    @sio.event
+    async def connect():
+        _LOGGER.info("Connected to WebSocket Server")
+
+    @sio.event
+    async def disconnect():
+        _LOGGER.info("Disconnected from WebSocket Server")
+
+    try:
+        await sio.connect(f'ws://{host}:{port}', namespaces=['/ws-color'])
+        return sio
+    except Exception as e:
+        _LOGGER.error(f"Failed to establish WebSocket connection: {e}")
+        return None
+
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the component."""
 
@@ -63,13 +85,17 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
         logger=_LOGGER,
         name="world_map_entity_manager",
         update_method=lambda: async_update_data(hass),
-        update_interval=timedelta(minutes=1),
+        update_interval=timedelta(minutes=5),
     )
+
+    # Setup WebSocket connection
+    websocket_client = await async_setup_websocket(hass, host, port)
 
     hass.data[DOMAIN] = {
         "coordinator": coordinator,
         "session": session,
         "API_URL": f"http://{host}:{port}",
+        "websocket": websocket_client
     }
 
     await coordinator.async_refresh()
@@ -83,14 +109,16 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     hass.services.async_register(DOMAIN, "create_entity", lambda call: handle_create_entity(call, session, hass), schema=CREATE_ENTITY_SCHEMA)
     hass.services.async_register(DOMAIN, "update_entity", lambda call: handle_update_entity(call, session, hass), schema=UPDATE_ENTITY_SCHEMA)
     hass.services.async_register(DOMAIN, "delete_entity", lambda call: handle_delete_entity(call, session, hass), schema=DELETE_ENTITY_SCHEMA)
-    hass.services.async_register(DOMAIN, "set_color", lambda call: handle_set_color(call, session, hass), schema=SET_COLOR_SCHEMA)
+    hass.services.async_register(DOMAIN, "set_color", lambda call: handle_set_color(call, hass), schema=SET_COLOR_SCHEMA)
     # Register other services similarly
 
-    async def async_close_session(event):
-        """Close aiohttp session on shutdown."""
-        await session.close()
+    async def async_close_websocket(event):
+        """Close WebSocket connection on shutdown."""
+        websocket_client = hass.data[DOMAIN]["websocket"]
+        if websocket_client:
+            await websocket_client.disconnect()
 
-    hass.bus.async_listen_once("homeassistant_stop", async_close_session)
+    hass.bus.async_listen_once("homeassistant_stop", async_close_websocket)
 
     return True
 
--- light.py
+++ light.py
@@ -1,4 +1,6 @@
 from homeassistant.components.light import LightEntity, ATTR_RGB_COLOR, ATTR_BRIGHTNESS
+from homeassistant.components.light import SUPPORT_BRIGHTNESS, SUPPORT_COLOR
+from homeassistant.util.color import color_hs_to_RGB
 from . import DOMAIN
 import logging
 import aiohttp
@@ -31,63 +33,135 @@ class EntityManagerLightEntity(LightEntity):
         self.session = session
 
     @property
+    def brightness(self):
+        """Return the brightness of the light."""
+        return self._entity_data["state"]["brightness"]
+    
+    @property
+    def rgb_color(self):
+        """Return the rgb color of the light."""
+        return self._entity_data["state"].get("rgb_color", (255, 255, 255))
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        return SUPPORT_BRIGHTNESS | SUPPORT_COLOR
+    
+    @property
     def is_on(self):
         """Return true if the light is on."""
         return self._entity_data["state"]["is_on"]
 
     async def async_turn_on(self, **kwargs):
-        """Turn on the light through the external API."""
-        turn_on_data = {
-            "entity": self._attr_unique_id,
-            "is_on": False
-        }
+        """Turn on the light."""
+        data = {"entity": self._attr_unique_id, "is_on": True}
 
-        try:
-            async with self.session.post(f"{self.api_url}/toggle/", json=turn_on_data) as response:
-                _LOGGER.error(f"Response: {response}")
-                _LOGGER.error(f"Response status: {response.status}")
-                _LOGGER.error(f"Response text: {await response.text()}")
-                _LOGGER.error(f"Response json: {await response.json()}")
-                if response.status == 200:
-                    response_data = await response.json()  # Correctly await the result
-                    if response_data["is_on"] == turn_on_data["is_on"]:
-                        self._entity_data["state"]["is_on"] = True
-                        _LOGGER.info(f"Turned on entity {self._attr_unique_id}")
-                    else:
-                        self._entity_data["state"]["is_on"] = response_data["is_on"]
-                        _LOGGER.error(f"Failed to turn on entity: {response_data}")
-                else:
-                    error_message = await response.text()
-                    _LOGGER.error(f"Failed to turn on entity: {error_message}")
-        except aiohttp.ClientError as e:
-            _LOGGER.error(f"Error communicating with API to turn on entity: {e}")
+        # Check if color or brightness is specified in kwargs and update data accordingly
+        if ATTR_RGB_COLOR in kwargs or ATTR_BRIGHTNESS in kwargs:
+            color_brightness_data = self._prepare_color_data(kwargs)
+            data.update(color_brightness_data)
+        else:
+            # Use existing color data if no color or brightness is specified
+            color_data = self._prepare_color_data(kwargs)
+            data.update(color_data)
+
+        await self._send_color_request(data)
 
     async def async_turn_off(self, **kwargs):
-        """Turn off the light through the external API."""
-        turn_off_data = {
-            "entity": self._attr_unique_id,
-            "is_on": False
+        """Turn off the light."""
+        data = {"entity": self._attr_unique_id, "is_on": False}
+        await self._send_color_request(data)
+
+    async def async_set_color(self, **kwargs):
+        """Set the color and brightness of the light."""
+        data = {"entity": self._attr_unique_id, "is_on": True}
+
+        if ATTR_RGB_COLOR in kwargs or ATTR_BRIGHTNESS in kwargs:
+            color_brightness_data = self._prepare_color_data(kwargs)
+            data.update(color_brightness_data)
+        else:
+            # Use existing color data if no color or brightness is specified
+            color_data = self._prepare_color_data(kwargs)
+            data.update(color_data)
+
+        await self._send_color_request(data)
+
+    def _prepare_color_data(self, kwargs):
+        """Prepare the current color and brightness data based on kwargs or current state."""
+        log = []
+        log.append(f"Prepare Color Data for entity {self._attr_unique_id}")
+        log.append(f"Initial kwargs: {kwargs}")
+
+        # Check if HS color is provided, then convert to RGB
+        if 'hs_color' in kwargs:
+            hs_color = kwargs['hs_color']
+            rgb_color = color_hs_to_RGB(*hs_color)
+        else:
+            rgb_color = self.rgb_color or (0, 0, 0)
+
+        log.append(f"RGB Color: {rgb_color}")
+        log.append(f"Red: {rgb_color[0]}")
+        log.append(f"Green: {rgb_color[1]}")
+        log.append(f"Blue: {rgb_color[2]}")
+
+        brightness = kwargs.get(ATTR_BRIGHTNESS, self.brightness or 0)
+
+        log.append(f"Brightness: {brightness}")
+
+        _LOGGER.error(log)
+
+        return {
+            "red": rgb_color[0],
+            "green": rgb_color[1],
+            "blue": rgb_color[2],
+            "brightness": 100 #int(brightness / 255 * 100)
         }
 
+    def _update_entity_data(self, data):
+        """Update the internal state of the entity."""
+        if "is_on" in data:
+            self._entity_data["state"]["is_on"] = data["is_on"]
+        if "red" in data and "green" in data and "blue" in data:
+            self._entity_data["state"]["rgb_color"] = (data["red"], data["green"], data["blue"])
+        if "brightness" in data:
+            # Scale up brightness to 0-255 range
+            self._entity_data["state"]["brightness"] = int(data["brightness"] / 100 * 255)
+
+    async def _send_color_request(self, data):
+        socketio_client = self.hass.data[DOMAIN]['websocket']
+        if socketio_client:
+            try:
+                # Emit the message to the Flask-SocketIO server
+                await socketio_client.emit('set_color', data, namespace='/ws-color')
+                _LOGGER.info(f"Sent color update via WebSocket for entity {self._attr_unique_id}")
+            except Exception as e:
+                _LOGGER.error(f"Error sending message via WebSocket: {e}, falling back to REST API")
+                await self._send_color_request_fallback(data)
+        else:
+            _LOGGER.error(f"No WebSocket connection available, falling back to REST API")
+            await self._send_color_request_fallback(data)
+
+    async def _send_color_request_fallback(self, data):
         try:
-            async with self.session.post(f"{self.api_url}/toggle/", json=turn_off_data) as response:
-                _LOGGER.error(f"Response: {response}")
-                _LOGGER.error(f"Response status: {response.status}")
-                _LOGGER.error(f"Response text: {await response.text()}")
-                _LOGGER.error(f"Response json: {await response.json()}")
+            async with self.session.post(f"{self.api_url}/color/", json=data) as response:
                 if response.status == 200:
-                    response_data = await response.json()  # Correctly await the result
-                    if response_data["is_on"] == turn_off_data["is_on"]:
-                        self._entity_data["state"]["is_on"] = False
-                        _LOGGER.info(f"Turned off entity {self._attr_unique_id}")
+                    response_data = await response.json()
+                    if response_data.get("success"):
+                        self._update_entity_data_from_response(response_data)
+                        _LOGGER.info(f"Updated color for entity {self._attr_unique_id}")
                     else:
-                        self._entity_data["state"]["is_on"] = response_data["is_on"]
-                        _LOGGER.error(f"Failed to turn off entity: {response_data}")
+                        _LOGGER.error(f"API response indicates failure: {response_data}")
                 else:
                     error_message = await response.text()
-                    _LOGGER.error(f"Failed to turn off entity: {error_message}")
+                    _LOGGER.error(f"Failed to set color for entity: {error_message}")
         except aiohttp.ClientError as e:
-            _LOGGER.error(f"Error communicating with API to turn off entity: {e}")
+            _LOGGER.error(f"Error communicating with API to set color: {e}")
+
+    def _update_entity_data_from_response(self, data):
+        """Update the internal state of the entity from API response."""
+        self._entity_data["state"]["is_on"] = data.get("is_on", self.is_on)
+        self._entity_data["state"]["rgb_color"] = (data.get("red", 0), data.get("green", 0), data.get("blue", 0))
+        self._entity_data["state"]["brightness"] = int(data.get("brightness", 0) / 100 * 255)
 
     async def async_update(self):
         """Update the entity."""
@@ -99,12 +173,3 @@ class EntityManagerLightEntity(LightEntity):
             self.coordinator.async_add_listener(self.async_write_ha_state)
         )
 
-    @property
-    def brightness(self):
-        """Return the brightness of the light."""
-        return self._entity_data["state"]["brightness"]
-    
-    @property
-    def rgb_color(self):
-        """Return the rgb color of the light."""
-        return self._entity_data["state"]["rgb_color"]
